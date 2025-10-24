798e703141bcede4ee72599096a97dc7ac378d4c
gumends <gustavo08.mendes@gmail.com>
2025-10-24 02:11:03 -0300

    Inserindo layout da pagina de produto

--- src/types/produto.type.ts
+++ src/types/produto.type.ts
@@ -3,12 +3,32 @@ export interface Produto {
   nomeProduto: string;
   valor: number;
   valorOriginal: number;
+  valorParcelado: number;
   desconto: number;
   descricao: string;
   tipoProduto: number;
   novoLançamento: boolean;
   novaGeracao?: boolean;
   modelos?: ModelosOption[];
+  disponivel: boolean;
+  mesesGarantia: number
+  informacoesAdicionais: Informacoes | null;
+}
+
+export interface Informacoes {
+  marca: string;
+  armazenamentoInterno: string | null;
+  tipoTela: string | null;
+  tamanhoTela: string | null;
+  resolucaoTela: string | null;
+  tecnologia: string | null;
+  processador: string | null;
+  sistemaOperacional: string | null;
+  cameraTraseira: string | null;
+  cameraFrontal: string | null;
+  bateria: string | null;
+  quantidadeChips: string | null;
+  material: string | null;
 }
 
 export interface Produtos {
